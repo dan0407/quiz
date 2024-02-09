5a09de4fed7195ed61d9d7465f62fea88e23a154
Daniel Alejandro Duque Murillas <1025060503@u.icesi.edu.co>
2024-02-09 17:34:31 -0500

    1

---
+++ index.js
@@ -0,0 +1,8 @@
+const notas={
+  {porcentaje "90 - 100", nota: A};
+  {porcentaje "80 - 89", nota: B};
+  {porcentaje "70 - 79", nota: c};
+  {porcentaje "60 - 69", nota:  D};
+  {porcentaje "60", nota: F};
+};
+
