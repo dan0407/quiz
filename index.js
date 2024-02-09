8ba079a7f7455ae2ff970ce9f2a3d226a5ba8dd8
Daniel Alejandro Duque Murillas <1025060503@u.icesi.edu.co>
2024-02-09 17:39:30 -0500

    2

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const notas={
-  {porcentaje "90 - 100", nota: A};
-  {porcentaje "80 - 89", nota: B};
-  {porcentaje "70 - 79", nota: c};
-  {porcentaje "60 - 69", nota:  D};
-  {porcentaje "60", nota: F};
-};
+const studentGrade = [100, 50, 65, 70, 85, 96, 45];
 
+const result = computerGrade(studentGrade);
+console.log(result);
