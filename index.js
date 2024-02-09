6d37f8f470f565ba8abdc82651a819dfe78aaf55
Daniel Alejandro Duque Murillas <1025060503@u.icesi.edu.co>
2024-02-09 17:49:51 -0500

    3

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const studentGrade = [100, 50, 65, 70, 85, 96, 45];
 
+for (let index = 0; index < studentGrade.length; index++) {
+	const element = studentGrade[index];
+}
+function esSuficientementeGrande(studentGrade) {
+	return elemento < 60;
+}
+var filtrados = [100, 50, 65, 70, 85, 96, 45].filter(studentGrade);
+
 const result = computerGrade(studentGrade);
 console.log(result);
